429c7486b3bd1a11809d80213110982faafea460
DANILAPTOP\danim <57731531+danielmartina@users.noreply.github.com>
2025-01-18 18:28:51 +0100

    Correct 7 zip path

--- scripts/create-qt-pdb-zip.bat
+++ scripts/create-qt-pdb-zip.bat
@@ -1,4 +1,4 @@
 rem Run from Qt command prompt without changing directories
 
 del symbols.zip
-7z a symbols.zip *.pdb -r -xr!Qt5WebEngineCore*
+"C:\Program Files\7-Zip\7z.exe" a symbols.zip *.pdb -r -xr!Qt5WebEngineCore*
